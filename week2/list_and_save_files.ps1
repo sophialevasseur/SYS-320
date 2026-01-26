42cf50bb2266cb5276c83359234ef1bc062b6347
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-26 17:38:58 -0500

    Create list_and_save_files.ps1

---
+++ week2/list_and_save_files.ps1
@@ -0,0 +1,7 @@
+$files = Get-ChildItem
+
+$folderPath = "$PSScriptRoot/outfolder/"
+$filePath = Join-Path $folderPath "out.csv"
+
+$files | Where-Object { $_.Extension -eq ".ps1" } |
+Export-Csv -Path $filePath
