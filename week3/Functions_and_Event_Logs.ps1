f49ec005c1e66ee4af562b477be238f9a7ccbccf
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-28 13:28:07 -0500

    Update Functions_and_Event_Logs.ps1

--- week3/Functions_and_Event_Logs.ps1
+++ week3/Functions_and_Event_Logs.ps1
@@ -1,5 +1,5 @@
 # Get login and logoff records from Windows Events
-Get-EventLog -system -source Microsoft-Windows-WinLogon
-
-# attempt 2: 
 Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing 
+
+# Get the last 14 days
+$loginouts = Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing -After (Get-Date) .AddDays (-14)
