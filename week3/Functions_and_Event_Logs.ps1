7efe6090c1bebe2f5af4048afeb1be9022dc12ec
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-28 13:33:45 -0500

    Update Functions_and_Event_Logs.ps1

--- week3/Functions_and_Event_Logs.ps1
+++ week3/Functions_and_Event_Logs.ps1
@@ -3,3 +3,4 @@ Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing
 
 # Get the last 14 days
 $loginouts = Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing -After (Get-Date) .AddDays (-14)
+Get-EventLog -LogName Security -After (Get-Date).AddDays(-14) | Where-Object {$_.EventID -in @(4624, 4634)}
