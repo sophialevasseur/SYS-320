0a855bce09fffb71f94f8fb345b32bbf73dc680a
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-28 13:53:25 -0500

    Update Functions_and_Event_Logs.ps1

--- week3/Functions_and_Event_Logs.ps1
+++ week3/Functions_and_Event_Logs.ps1
@@ -1,6 +1,34 @@
+# DELIVERABLE 1 (Lines 2-3): 
 # Get login and logoff records from Windows Events
 Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing 
 
+# DELIVERABLE 2 (Lines 6-34):
 # Get the last 14 days
-$loginouts = Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing -After (Get-Date) .AddDays (-14)
 Get-EventLog -LogName Security -After (Get-Date).AddDays(-14) | Where-Object {$_.EventID -in @(4624, 4634)}
+
+# Create an empty array to store custom objects
+# The @() creates an empty table we can add objects to
+$loginoutsTable = @()
+
+# Loop through each event
+# This table loops from 0 to the total number of events
+for($i=0; $i -lt $loginouts.Count; $i++){
+   
+}
+
+# Determine whether it is a logon or logoff value
+  $event = ""
+  if($loginouts[$i].InstanceId -eq 7001) {$event="Logon"}
+  if($loginouts[$i].InstanceId -eq 7002) {$event="Logoff"}
+  
+# Extract the username from the string
+  $user = $loginouts[$i].ReplacementStrings[1]
+
+# Add 4 properties to the array
+  $loginoutsTable += [PSCustomObject]@{
+    "Time" = $loginouts[$i].TimeGenerated
+    "Id" = $loginouts[$i].InstanceId  
+    "Event" = $event
+    "User" = $user
+  }
+}
