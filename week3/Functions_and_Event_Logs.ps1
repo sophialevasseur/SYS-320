46cbdf4d898c21c20d2fe8c31f02014ea35914b2
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-28 14:30:41 -0500

    Update Functions_and_Event_Logs.ps1

--- week3/Functions_and_Event_Logs.ps1
+++ week3/Functions_and_Event_Logs.ps1
@@ -1,8 +1,7 @@
-# DELIVERABLE 1 (Lines 2-3): 
 # Get login and logoff records from Windows Events
 Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing 
 
-# DELIVERABLE 2 (Lines 6-34):
+# DELIVERABLES:
 # Get the last 14 days
 Get-EventLog -LogName Security -After (Get-Date).AddDays(-14) | Where-Object {$_.EventID -in @(4624, 4634)}
 
@@ -24,6 +23,15 @@ for($i=0; $i -lt $loginouts.Count; $i++){
 # Extract the username from the string
   $user = $loginouts[$i].ReplacementStrings[1]
 
+    # Convert SID to username
+    try {
+        $objSID = New-Object System.Security.Principal.SecurityIdentifier($sid)
+        $user = $objSID.Translate([System.Security.Principal.NTAccount]).Value
+    } catch {
+        # If translation fails, just use the SID
+        $user = $sid
+    }
+    
 # Add 4 properties to the array
   $loginoutsTable += [PSCustomObject]@{
     "Time" = $loginouts[$i].TimeGenerated
@@ -31,4 +39,5 @@ for($i=0; $i -lt $loginouts.Count; $i++){
     "Event" = $event
     "User" = $user
   }
-}
+
+
