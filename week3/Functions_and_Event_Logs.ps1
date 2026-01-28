c24eec396a4bcf01f74ebe848ef708cab7adb625
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-01-28 13:20:43 -0500

    Update Functions_and_Event_Logs.ps1

--- week3/Functions_and_Event_Logs.ps1
+++ week3/Functions_and_Event_Logs.ps1
@@ -1,2 +1,5 @@
 # Get login and logoff records from Windows Events
 Get-EventLog -system -source Microsoft-Windows-WinLogon
+
+# attempt 2: 
+Get-EventLog -LogName Security -source Microsoft-Windows-Security-Auditing 
