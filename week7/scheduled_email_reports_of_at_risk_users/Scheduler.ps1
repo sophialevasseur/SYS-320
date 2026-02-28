417ab21e795faa5fed973f76761fe40b81583258
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-02-28 15:07:19 -0500

    Add PowerShell script for scheduling email reports

---
+++ week7/scheduled_email_reports_of_at_risk_users/Scheduler.ps1
@@ -0,0 +1,39 @@
+function ChooseTimeToRun($Time){
+
+$scheduledTasks = Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
+
+if($scheduledTasks -ne $null){
+    Write-Host "The task already exists." | Out-String
+    DisableAutoRun
+}
+
+Write-Host "Creating new task." | Out-String 
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" `
+          -Argument "-File `"C:\Users\champuser\\SYS-320\week7\main.ps1`""
+
+$trigger = New-ScheduledTaskTrigger -Daily -At $Time
+$principal = New-ScheduledTaskPrincipal -UserId 'champuser' -RunLevel Highest
+$settings = New-ScheduledTaskSettingSet -RunOnlyIfNetworkAvailable -WakeToRun
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+
+Register-ScheduledTask 'myTask' -InputObject $task
+
+Get-ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
+
+}
+
+function DisableAutoRun(){
+
+$scheduledTasks = Get=ScheduledTask | Where-Object { $_.TaskName -ilike "myTask" }
+
+if($scheduledTasks -ne $null){
+    Write-Host "Unregistering the task." | Out-String
+    Unregister-ScheduledTask -TaskName 'myTask' -Confirm:$false
+}
+
+else{
+    Write-Host "The task is not registered." | Out-String
+}
+
+}
