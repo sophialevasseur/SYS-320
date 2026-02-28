7b75a3f7c81bf2cc9f31a04ddd1f8b86b1c86c68
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-02-28 14:26:06 -0500

    Change password handling to use text file input
    Updated password retrieval method to read from a text file.

---
+++ week7/scheduled_email_reports_of_at_risk_users/Encrypted_email.ps1
@@ -0,0 +1,15 @@
+function SendAlertEmail($Body){
+
+$From = "Add-Champ-Email"
+$To = "Add-Champ-Email"
+$Subject = "Suspicious Activity"
+
+$Password = Get-Content "C:\Users\champuser\...filepathtotextfile" | ConvertTo-SecureString
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
+
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" `
+-port 587 -UseSsl -Credential $Credential
+
+}
+
+SendAlertEmail "Body of email"
