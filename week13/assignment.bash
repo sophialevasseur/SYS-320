74ab8df6cc6e83a9c1384a2c32925be69e45214e
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-04-16 13:20:42 -0400

    Refactor data processing in assignment.bash
    Fix variable names and improve loop structure for data processing.

--- week13/assignment.bash
+++ week13/assignment.bash
@@ -18,16 +18,28 @@ timedata=$(echo "$fullPage" | \
 xmlstarlet select --template --value-of \
 "//table[@id='temp']//tr//td[2]")
 
+lineCount=$(echo "$tempData" | wc -l)
+
+# loop by number of lines and print data accordingly
+for (( i=1; i<="$lineCount"; i++ ));
+do
+	temp=$(echo "$tempData" | head -n $i | tail -n 1)
+	pressure=$(echo "$pressureData" | head -n $i | tail -n 1)
+	times=$(echo "$timeData" | head -n $i | tail -n 1)
+
+	echo -e "$pressure $temp $times"
+done
+
 #linecount=$(echo "$tempdata" | wc -1 )
 
-for (( sed 's/ / | /g' ));
-do
-	echo "Here is deliverable 1!"
+#for (( sed 's/ / | /g' ));
+#do
+#	echo "Here is deliverable 1!"
 
-	temp=$(echo "$tempdata" | head -n $i | tail -n 1 )
-	pressure=$(echo "$pressuredata" | head -n $i | tail -n 1 )
-	times=$(echo "$timedata" | head -n $i | tail -n 1 )
+#	temp=$(echo "$tempdata" | head -n $i | tail -n 1 )
+#	pressure=$(echo "$pressuredata" | head -n $i | tail -n 1 )
+#	times=$(echo "$timedata" | head -n $i | tail -n 1 )
 
-	echo -e "$temp $pressure $times"
+#	echo -e "$temp $pressure $times"
 
-done
+#done
