af3b2ffe93fab3889db74d112c3824ddb6625d09
sophialevasseur <sophia.levasseur@mymail.champlain.edu>
2026-04-16 13:30:25 -0400

    modified:   assignment.bash

--- week13/assignment.bash
+++ week13/assignment.bash
@@ -18,28 +18,14 @@ timedata=$(echo "$fullPage" | \
 xmlstarlet select --template --value-of \
 "//table[@id='temp']//tr//td[2]")
 
-lineCount=$(echo "$tempData" | wc -l)
+linecount=$(echo "$tempdata" | wc -l)
 
-# loop by number of lines and print data accordingly
-for (( i=1; i<="$lineCount"; i++ ));
-do
-	temp=$(echo "$tempData" | head -n $i | tail -n 1)
-	pressure=$(echo "$pressureData" | head -n $i | tail -n 1)
-	times=$(echo "$timeData" | head -n $i | tail -n 1)
+# loop by number of lines and print
+for (( i=1; i<=$linecount; i++ )); do
+
+	temp=$(echo "$tempdata" | head -n $i | tail -n 1)
+	pressure=$(echo "$pressuredata" | head -n $i | tail -n 1)
+	times=$(echo "$timedata" | head -n $i | tail -n 1)
 
 	echo -e "$pressure $temp $times"
 done
-
-#linecount=$(echo "$tempdata" | wc -1 )
-
-#for (( sed 's/ / | /g' ));
-#do
-#	echo "Here is deliverable 1!"
-
-#	temp=$(echo "$tempdata" | head -n $i | tail -n 1 )
-#	pressure=$(echo "$pressuredata" | head -n $i | tail -n 1 )
-#	times=$(echo "$timedata" | head -n $i | tail -n 1 )
-
-#	echo -e "$temp $pressure $times"
-
-#done
